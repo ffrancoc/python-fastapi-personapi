ff9256701595826572adab643fbe8fb7c689e0eb
Francisco <jose2fcurin@gmail.com>
2022-10-21 12:09:58 -0600

    Implementacion de las rutas GET para obtener todos los registros y el registro mediante id

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from db import Database
 
 app = FastAPI()
 
@@ -6,4 +7,19 @@ app = FastAPI()
 def root():
     return {'mensaje': 'Simple Api con sqlite3'}
 
-
+# Devolver todas las personas de la base de datos
+@app.get('/personas')
+def read_personas():
+    return Database.read_personas()
+    
+# Devolver persona de la base de datos por id mediante parametros url
+@app.get('/persona')
+def read_person_by_id_param(id:int):
+    persona = Database.read_person_by_id(id)
+    return persona if persona else []
+    
+# Devolver persona de la base de datos por id mediante url
+@app.get('/persona/id/{id}')
+def read_person_by_id(id:int):
+    persona = Database.read_person_by_id(id)
+    return persona if persona else []
