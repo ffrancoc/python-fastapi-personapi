06dd2b12c6d545a65d3e0ff45f0db6397be4c14d
Francisco <jose2fcurin@gmail.com>
2022-10-22 19:29:02 -0600

    Creacion del metodo GET para obtener el/los usuarios con el nombre dado

--- db.py
+++ db.py
@@ -1,5 +1,7 @@
+# import logging
 import sqlite3 as sqlite
 from decouple import config
+from filelog import FileLog
 
 # Obtener la ruta de la base de datos del archivo .env
 DATABASE = config('DATABASE')
@@ -12,6 +14,7 @@ class Database:
         if cur: cur.close()
         if conn: conn.close()
     
+
     # Obtener todos los datos de todas las personas en la base de datos
     @staticmethod
     def read_personas()->list:
@@ -22,10 +25,12 @@ class Database:
             cur = conn.cursor()
             personas = cur.execute('SELECT * FROM person').fetchall()            
         except Exception as ex:
-            print('Error: %s' % ex)
+            # logging.exception('Error: %s', ex)
+            FileLog.save_message('/persons/', ex)
         finally:
             Database.close_connection(cur, conn)            
         return personas
+
 
     # Obtener persona por id
     @staticmethod
@@ -37,8 +42,25 @@ class Database:
             cur = conn.cursor()
             persona = cur.execute('SELECT * FROM person WHERE id=:id', {'id':id}).fetchone()        
         except Exception as ex:
-            print('Error: %s' % ex)
+            # logging.exception('Error: %s', ex)
+            FileLog.save_message('(/person/id/{id}|/person?id=)', ex)
         finally:
             Database.close_connection(cur, conn)
         return persona
 
+
+    @staticmethod
+    def read_persons_by_nombre(nombre:str)->list:
+        personas = []
+        try:
+            conn = sqlite.connect(DATABASE)
+            conn.row_factory= sqlite.Row
+            cur = conn.cursor()
+            personas = cur.execute('SELECT * FROM person WHERE nombre= :nombre', {'nombre': nombre}).fetchall()
+        except Exception as ex:
+            # logging.exception('Error: %s', ex)
+            FileLog.save_message('/persons/nombre/{nombre}', ex)
+        finally:
+            Database.close_connection(cur, conn)
+        return personas
+        
