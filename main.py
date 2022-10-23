7f490e7937c4d360abc65c0ea7ab5c0aeb0a2714
Francisco <jose2fcurin@gmail.com>
2022-10-22 19:31:37 -0600

    Implemetacion de la ruta GET para obtener el/los registro(s) por nombre

--- main.py
+++ main.py
@@ -7,17 +7,26 @@ app = FastAPI()
 def root():
     return {'mensaje': 'Simple Api con sqlite3'}
 
+
 # Devolver todas las personas de la base de datos
 @app.get('/personas')
 def read_personas():
     return Database.read_personas()
-    
+
+
+# Devolver personas que empiezen con el nombre enviado
+@app.get('/personas/nombre/{nombre}')
+def read_persons_by_nombre(nombre:str):
+    return Database.read_persons_by_nombre(nombre)
+
+
 # Devolver persona de la base de datos por id mediante parametros url
 @app.get('/persona')
 def read_person_by_id_param(id:int):
     persona = Database.read_person_by_id(id)
     return persona if persona else []
-    
+
+
 # Devolver persona de la base de datos por id mediante url
 @app.get('/persona/id/{id}')
 def read_person_by_id(id:int):
